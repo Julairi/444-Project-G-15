2ac5fe598d69e7611501c0f4fced7b0bd4de0e7c
Reema <ms.remaim@gmail.com>
2022-09-15 09:33:12 +0300

    remove conflicts

--- lib/model/company_model.dart
+++ lib/model/company_model.dart
@@ -2,47 +2,23 @@ class CompanyModel {
   String? cid;
   String? email;
   String? Name;
-<<<<<<< HEAD
-
+  String? imgUrl;
   String? description;
   String? contact;
   String? address;
   String? role;
-  //bool? company;
-  // bool? jobseeker;
   // String? phone;
   //String? experience;
   CompanyModel({
     this.cid,
     this.email,
+    this.imgUrl,
     this.Name,
     this.contact,
     this.description,
     this.address,
     this.role,
-    // this.company,
-    //this.jobseeker
   });
-=======
-  String? imgUrl;
-  String? description;
-  String? contact;
-  String? address;
-  bool? company;
-  bool? jobseeker;
-  // String? phone;
-  //String? experience;
-  CompanyModel(
-      {this.cid,
-      this.email,
-      this.imgUrl,
-      this.Name,
-      this.contact,
-      this.description,
-      this.address,
-      this.company,
-      this.jobseeker});
->>>>>>> 2ab1a8cea3bb7cf9ecd7b2c4e1a58514cb4490f4
 
   // receiving data from server
   factory CompanyModel.fromMap(map) {
@@ -70,22 +46,13 @@ class CompanyModel {
       'cid': cid,
       'email': email,
       'Name': Name,
-<<<<<<< HEAD
-=======
-      'imgUrl': imgUrl, //++
->>>>>>> 2ab1a8cea3bb7cf9ecd7b2c4e1a58514cb4490f4
       'description': description,
       'contact': contact,
       //'gender': gender,
       'adress': address,
-<<<<<<< HEAD
       'role': 'company'
       //'company': 'true',
       //'jobseeker': 'false',
-=======
-      'company': 'true',
-      'jobseeker': 'false',
->>>>>>> 2ab1a8cea3bb7cf9ecd7b2c4e1a58514cb4490f4
 
       //'phone': phone,
       //'experience': experience,
