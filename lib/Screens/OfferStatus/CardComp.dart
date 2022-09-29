c83786c0e866b7248d0481ca4ebe8800ce3a2b45
reemafahad100 <reemaaaaaaaaa.777@gmail.com>
2022-09-30 02:20:22 +0300

    new

--- lib/Screens/OfferStatus/CardComp.dart
+++ lib/Screens/OfferStatus/CardComp.dart
@@ -29,146 +29,157 @@ class CardComp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         child: InkWell(
-            //    onTap: () {
-            //        Navigator.push(
-            //        context,
-            //       MaterialPageRoute(
-            //            builder: (
-            //         context,
-            //       ) =>
-            //               IntrestedJs(postid: UID)));
-            //  },
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (
+                    context,
+                  ) =>
+                          DetailsPageForC(
+                              uid: UID,
+                              CompanyPath: CompanyPath,
+                              CompanyName: CompanyName,
+                              offertitle: offertitle,
+                              offerCity: offerCity,
+                              offerDate: offerDate,
+                              offerDes: offerDes,
+                              offerFee: offerFee,
+                              offerTime: offerTime,
+                              offerHours: offerHours)));
+            },
             child: Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      elevation: 7,
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
-                ),
-                child: Image.network(
-                  "https://i.pinimg.com/474x/6a/d3/66/6ad3663d79ccc962377d7a6cbe4d9bfe.jpg",
-                  height: 50,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
               ),
-              Positioned(
-                child: Container(
-                  height: 50,
-                  alignment: Alignment.bottomRight,
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                        Color.fromARGB(255, 105, 110, 112).withOpacity(0),
-                        Color.fromARGB(255, 64, 69, 71).withOpacity(0.2)
-                      ],
-                          stops: [
-                        0.6,
-                        1
-                      ])),
-                  child: Row(
+              elevation: 7,
+              margin: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Stack(
                     children: [
-                      SizedBox(
-                        height: 20,
-                        width: 20,
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15),
+                        ),
+                        child: Image.network(
+                          "https://i.pinimg.com/474x/6a/d3/66/6ad3663d79ccc962377d7a6cbe4d9bfe.jpg",
+                          height: 50,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      Icon(
-                        Icons.work_outline,
-                        color: Color.fromARGB(255, 83, 80, 80),
-                        size: 35,
-                      ),
-                      SizedBox(
-                        height: 20,
-                        width: 20,
-                      ),
-                      Text(
-                        offertitle,
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 6, 6, 6),
-                            fontSize: 18,
-                            fontFamily: 'ElMessiri',
-                            fontWeight: FontWeight.bold,
-                            overflow: TextOverflow.fade),
+                      Positioned(
+                        child: Container(
+                          height: 50,
+                          alignment: Alignment.bottomRight,
+                          padding: EdgeInsets.symmetric(
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [
+                                Color.fromARGB(255, 105, 110, 112)
+                                    .withOpacity(0),
+                                Color.fromARGB(255, 64, 69, 71).withOpacity(0.2)
+                              ],
+                                  stops: [
+                                0.6,
+                                1
+                              ])),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                height: 20,
+                                width: 20,
+                              ),
+                              Icon(
+                                Icons.work_outline,
+                                color: Color.fromARGB(255, 83, 80, 80),
+                                size: 35,
+                              ),
+                              SizedBox(
+                                height: 20,
+                                width: 20,
+                              ),
+                              Text(
+                                offertitle,
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 6, 6, 6),
+                                    fontSize: 18,
+                                    fontFamily: 'ElMessiri',
+                                    fontWeight: FontWeight.bold,
+                                    overflow: TextOverflow.fade),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: Color.fromARGB(255, 237, 229, 109),
+                            ),
+                            SizedBox(
+                              height: 20,
+                              width: 10,
+                            ),
+                            Text(offerCity,
+                                style: TextStyle(
+                                    color: kPrimaryColor,
+                                    fontSize: defaultFontSize,
+                                    fontWeight: FontWeight.bold,
+                                    overflow: TextOverflow.fade))
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                          width: 15,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.business,
+                              color: Color.fromARGB(255, 3, 77, 138),
+                            ),
+                            SizedBox(
+                              height: 20,
+                              width: 10,
+                            ),
+                            GestureDetector(
+                                child: Text("طلبات هذا العرض",
+                                    style: TextStyle(
+                                        color: Colors.blue,
+                                        fontSize: defaultFontSize,
+                                        fontWeight: FontWeight.bold,
+                                        decoration: TextDecoration.underline,
+                                        overflow: TextOverflow.ellipsis)),
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      new MaterialPageRoute(
+                                          builder: (context) => new IntrestedJs(
+                                                postid: UID,
+                                              )));
+                                })
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.location_on,
-                      color: Color.fromARGB(255, 237, 229, 109),
-                    ),
-                    SizedBox(
-                      height: 20,
-                      width: 10,
-                    ),
-                    Text(offerCity,
-                        style: TextStyle(
-                            color: kPrimaryColor,
-                            fontSize: defaultFontSize,
-                            fontWeight: FontWeight.bold,
-                            overflow: TextOverflow.fade))
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                  width: 15,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.business,
-                      color: Color.fromARGB(255, 3, 77, 138),
-                    ),
-                    SizedBox(
-                      height: 20,
-                      width: 10,
-                    ),
-                    GestureDetector(
-                        child: Text("طلبات هذا العرض",
-                            style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: defaultFontSize,
-                                fontWeight: FontWeight.bold,
-                                decoration: TextDecoration.underline,
-                                overflow: TextOverflow.ellipsis)),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              new MaterialPageRoute(
-                                  builder: (context) => new IntrestedJs(
-                                        postid: UID,
-                                      )));
-                        })
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    )));
+            )));
   }
 }
