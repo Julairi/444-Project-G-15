5bad202169bb69fcf24aed7e7b650e0628606840
Khawlah Alghanim <104341910+khwla12@users.noreply.github.com>
2022-11-03 08:03:56 +0000

    splash

--- essa/esaa/lib/screens/intro/splash.dart
+++ essa/esaa/lib/screens/intro/splash.dart
@@ -26,6 +26,64 @@ class Splash extends StatelessWidget {
         exit(0);
       },
       child: Scaffold(
+        backgroundColor: kFillColor,
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/header.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                height: 180,
+                width: 300,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const CircularProgressIndicator(
+                color: kPrimaryColor,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+/*
+import 'dart:async';
+import 'dart:io';
+
+import 'package:esaa/config/constants.dart';
+import 'package:esaa/services/services.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      if (Auth().isSignedIn) {
+        Get.toNamed('/');
+      } else {
+        Get.toNamed('/login_screen');
+      }
+    });
+
+    return WillPopScope(
+      onWillPop: () async {
+        exit(0);
+      },
+      child: Scaffold(
         backgroundColor: kPrimaryColor,
         body: Center(
           child: Column(
@@ -47,4 +105,4 @@ class Splash extends StatelessWidget {
       ),
     );
   }
-}
+}*/
